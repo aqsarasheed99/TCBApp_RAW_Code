b267a6ac5c673593c90e5f06b735d6f43963c9f8
Aqsa099 <aqsachoudhary402@gmail.com>
2017-12-30 22:46:54 +0500

    purchase_invoice_insert.php file insert the new invoice

--- TCBApp Dashboard/production/purchase_invoice_insert.php
+++ TCBApp Dashboard/production/purchase_invoice_insert.php
@@ -4,13 +4,9 @@
 			
 			$distributer_id = $_POST['distributer_id'];
 			$date = $_POST['date'];
-			$amount_paid = $_POST['amount_paid'];
-			$amount_payable = $_POST['amount_payable'];
-			$discount_received = $_POST['discount_received'];	
-			$net_total= $_POST['net_total'];
-			$comment= $_POST['comment'];
+		    $comment= $_POST['comment'];
 				
 		//insert query
 		$insert = new crudop();
-		$insert->insert($distributer_id,$date,$amount_paid,$amount_payable, $discount_received,$net_total,$comment);
+		$insert->insert($distributer_id,$date,$comment);
 ?>
